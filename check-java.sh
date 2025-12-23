ca2cfdfe48282bdfc1bf77d2fb5a2608e21e2ee3
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-12-23 17:44:57 -0500

    log

--- check-java.sh
+++ check-java.sh
@@ -128,7 +128,7 @@ if [ -n "$JAVA_HOME" ] ; then
     if [ ! -x "$JAVACMD" ] ; then
         die "ERROR: JAVA_HOME is set to an invalid directory: $JAVA_HOME
 
-Please set the JAVA_HOME variable in your environment to match the
+ABC Please set the JAVA_HOME variable in your environment to match the
 location of your Java installation."
     fi
 else
@@ -137,7 +137,7 @@ else
     then
         die "ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 
-Please set the JAVA_HOME variable in your environment to match the
+CDE Please set the JAVA_HOME variable in your environment to match the
 location of your Java installation."
     fi
 fi
